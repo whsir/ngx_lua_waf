a9b370862407123465ffe3791a024b8f0c5fbae2
whsir <wh@whsir.com>
2019-04-03 14:37:18 +0800

    修改默认CC规则一分钟100

--- config.lua
+++ config.lua
@@ -10,7 +10,7 @@ black_fileExt={"php","jsp"}
 ipWhitelist={"127.0.0.1","192.168.1.0-192.168.1.255"}
 ipBlocklist={"1.0.0.1","2.0.0.0-2.0.0.255"}
 CCDeny="on"
-CCrate="1/60"
+CCrate="100/60"
 html=[[
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
